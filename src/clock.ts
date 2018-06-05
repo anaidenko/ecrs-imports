984521bf8ee90d92ff8c2925ab2a97d95f815a3b
anaidenko <Andrii.Naidenko@outlook.com>
2018-06-05 20:42:42 +0200

    Optional scheduler.

--- src/clock.ts
+++ src/clock.ts
@@ -2,9 +2,15 @@ import { CronJob } from 'cron'
 import * as config from './config'
 import JaxDataImporter from './importers/JaxDataImporter'
 
-export default new CronJob({
-  cronTime: config.CronTimerInterval,
-  onTick: new JaxDataImporter().run,
-  start: true,
-  timeZone: config.Timezone
-})
+let job: any = undefined
+
+if (config.CronTimerInterval) {
+  job = new CronJob({
+    cronTime: config.CronTimerInterval,
+    onTick: new JaxDataImporter().run,
+    start: true,
+    timeZone: config.Timezone
+  })
+}
+
+export default job
