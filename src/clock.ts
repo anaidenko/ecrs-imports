625bd681fd518112bba9d3c6d873a01a43366f10
anaidenko <Andrii.Naidenko@outlook.com>
2018-06-07 23:18:04 +0200

    Log scheduled task.

--- src/clock.ts
+++ src/clock.ts
@@ -1,13 +1,18 @@
 import { CronJob } from 'cron'
 import * as config from './config'
+import logger from './utils/logger'
 import JaxDataImporter from './importers/JaxDataImporter'
 
 let job: any = undefined
 
 if (config.CronTimerInterval) {
+  logger.log(`Starting timer via cron: ${config.CronTimerInterval}`)
   job = new CronJob({
     cronTime: config.CronTimerInterval,
-    onTick: new JaxDataImporter().run,
+    onTick: () => {
+      logger.log(`Starting scheduled jax importer on timer... ${config.CronTimerInterval}`)
+      return new JaxDataImporter().run()
+    },
     start: true,
     timeZone: config.Timezone
   })
